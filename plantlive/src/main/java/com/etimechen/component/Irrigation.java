e01df872c2b882ab74121fc6151a6282ade78dac
etimechen <etimechen@gmail.com>
2016-10-06 14:20:02 +0800

    over

--- plantlive/src/main/java/com/etimechen/component/Irrigation.java
+++ plantlive/src/main/java/com/etimechen/component/Irrigation.java
@@ -1,25 +1,49 @@
 package com.etimechen.component;
 
-import org.apache.log4j.Logger;
+import java.io.IOException;
 
 import com.etimechen.component.constant.ConfigConstant;
 
 public class Irrigation {
 	
-	private static final Logger logger = Logger.getLogger(Irrigation.class);
+//	private static final Logger logger = Logger.getLogger(Irrigation.class);
+	private static final String[] START = {"sudo","/home/pi/project/RPi_Relay_Board/shell/Relay.sh","CH3","ON"};
+	private static final String[] STOP = {"sudo","/home/pi/project/RPi_Relay_Board/shell/Relay.sh","CH3","OFF"};
 	
 	/**
 	 * 浇花
 	 */
 	public static void start(){
-		logger.info("开始浇花");
+//		logger.info("开始浇花");
+		Process pro;
+		try {
+			pro = Runtime.getRuntime().exec(START);
+	        pro.waitFor();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
 	}
 	
 	/**
 	 * 停止浇花
 	 */
 	public static void stop(){
-		logger.info("停止浇花");
+//		logger.info("停止浇花");
+		Process pro;
+		try {
+			pro = Runtime.getRuntime().exec(STOP);
+	        pro.waitFor();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
 	}
 	
 	/**
--- plantlive/src/main/java/com/etimechen/task/Task.java
+++ plantlive/src/main/java/com/etimechen/task/Task.java
@@ -5,7 +5,6 @@ import java.util.Date;
 
 import javax.annotation.Resource;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import com.etimechen.component.Configurator;
@@ -15,27 +14,27 @@ import com.etimechen.service.IVoteService;
 @Component
 public class Task {
 	
-	private static final Logger logger = Logger.getLogger(Task.class);
+//	private static final Logger logger = Logger.getLogger(Task.class);
 	private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 	
 	@Resource
 	private IVoteService voteService;
 	
 	public void irrigation(){		
-		logger.info("灌溉任务执行开始");
+//		logger.info("灌溉任务执行开始");
 		Date date = new Date();		
 		String nowHourMinutes = sdf.format(date);
 		if(nowHourMinutes.equals(Configurator.EXCUTE_TIME)){
-			logger.info("时间到了,开始一个浇水动作");
+//			logger.info("时间到了,开始一个浇水动作");
 			Boolean isExecute= this.voteService.statisticsVoteResult();
 			if(isExecute){
 				Irrigation.dooneperiod();
 			}
 		}else{
-			logger.info(nowHourMinutes);
-			logger.info(Configurator.EXCUTE_TIME);
-			logger.info("时间还没到");
+//			logger.info(nowHourMinutes);
+//			logger.info(Configurator.EXCUTE_TIME);
+//			logger.info("时间还没到");
 		}
-		logger.info("灌溉任务执行结束");
+//		logger.info("灌溉任务执行结束");
 	}
 }
